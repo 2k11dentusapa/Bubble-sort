4df31766af62ac68e3d62bb45645e2fcb707b05e
2k11dentusapa <nguyentruongthaogiang@gmail.com>
2026-01-22 21:38:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const arr = new Array(1, 6, 36, 322, 26, 34, 3456, 63, 7777, 5, 44734563, 6775675, 334, 3556, 3, 43556, 4355456, 435, 4); /* Có thể thay mảng thành mảng của bạn */
 let c;
 for (let i=0; i< arr.length; i++) {
-  for (let t=0; t< arr.length; t++) {
+  for (let t=0; t< arr.length - i - 1; t++) {
     if (arr[t] > arr[t+1]) {
       c = arr[t+1];
       arr[t+1] = arr[t];
