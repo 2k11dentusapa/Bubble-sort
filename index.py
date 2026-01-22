5451c2aee5c70dd18bbd5ad4bbe758a2eee80eb0
2k11dentusapa <nguyentruongthaogiang@gmail.com>
2026-01-22 21:33:15 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 arr = [1, 6, 36, 322, 26, 34, 3456, 63, 7777, 5, 44734563, 6775675, 334, 3556, 3, 43556, 4355456, 435, 4] # Có thể thay mảng thành mảng của bạn 
-
 n = len(arr)
 for i in range(n):
     for t in range(0, n - i - 1):
