5b7f9cc19180d94dc4ecead35da4bed694172ab9
2k11dentusapa <nguyentruongthaogiang@gmail.com>
2026-01-22 21:38:36 +0700

    Update index.java

--- index.java
+++ index.java
@@ -1,7 +1,7 @@
     public static void main(String[] args) {
       int[] x = {1, 6, 36, 322, 26, 34, 3456, 63, 7777,5, 44734563, 6775675, 334, 3556, 3, 43556 , 4355456, 435, 4}; /* có thể thay mảng thành mảng của bạn */
       for (int i=0; i<x.length; i++) {
-        for (int t=0; t<x.length - 1; t++) {
+        for (int t=0; t<x.length - i - 1; t++) {
           int c;
           if (x[t] > x[t+1]) {
             c = x[t+1];
